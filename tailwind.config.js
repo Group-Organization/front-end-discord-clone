d8e5dbaa48e962001360e607c6b64cfed0b16c69
Omolara5861 <debbiegterra@gmail.com>
2022-10-12 22:39:34 +0100

    ci: :wrench: update tailwind config file
    extend tailwind screens and max-width properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,19 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens: {
+
+      'md': '768px',
+      'lg': '1024px',
+    },
     colors: {
       'primary': '#404eed',
       'brand': '#5865f2',
       'notquiteblack': '#23272a',
       'offwhite': '#f6f6f6'
+    },
+    maxWidth: {
+      'page-max-width': '1260px',
     }
   },
   plugins: [],
